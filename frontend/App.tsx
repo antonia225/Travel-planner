42d299ea0348ccfd4992ea1bb92fbe7b8488a9c7
Elena-Antonia Stoica <antonia.e.stoica@gmail.com>
2026-05-12 19:00:20 +0300

    Add: frontend

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -20,7 +20,6 @@ function AuthStack() {
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
-        animationEnabled: true,
       }}
     >
       {isLogin ? (
@@ -94,7 +93,7 @@ export default function App() {
   return (
     <AuthProvider>
       <RootNavigator />
-      <StatusBar barStyle="light-content" />
+      <StatusBar style="light" />
     </AuthProvider>
   );
 }
